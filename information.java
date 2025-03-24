84162ee58fd751f56aa55c4c47d2f407ab95ecb1
rohanj0045 <121187117+rohanj0045@users.noreply.github.com>
2025-03-24 20:46:57 +0530

    Add files via upload

---
+++ information.java
@@ -0,0 +1,16 @@
+public class information
+{
+   public static void main(String[] args)
+{
+  System.out.println("Name     - ROHAN ASHOK JADHAV");
+  System.out.println("Age      - 21");
+  System.out.println("phone    - 9325913133");
+  System.out.println("Emali    - rohanjadhav23112003@gmail.com");
+  System.out.println("city     - Sangli");
+  System.out.println("Brithdate- 23/11/2003");
+  System.out.println("gender   - Male"); 
+  System.out.println("Education- Btech (Computer engineering)");
+
+}
+
+}
